30dd2e50976d3e946ad07fe33fd91883d6df9e5d
raheel0316 <raheelanwaar0316@gmail.com>
2023-12-04 12:48:03 -0800

    into the card

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -646,27 +646,36 @@
         {{-- contact us --}}
         <div class="text-center hidden" id="contact">
             contact us
-
-            <form action="mailto:asraah455@gmail.com?subject=Mail from Site!" method="post">
-                @csrf
-                <div class="form-group">
-                    <label>Name</label>
-                    <input type="text" name="name" placeholder="Enter Your Name">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <form action="mailto:asraah455@gmail.com?subject=Mail from Site!" method="post">
+                                    @csrf
+                                    <div class="form-group">
+                                        <label>Name</label>
+                                        <input type="text" name="name" placeholder="Enter Your Name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Subject</label>
+                                        <input type="text" name="subject" placeholder="Enter Your Subject">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Email</label>
+                                        <input type="email" name="email" placeholder="Enter Your Email">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Massage</label>
+                                        <textarea name="massage" cols="10" rows="10" class="form-control"></textarea>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label>Subject</label>
-                    <input type="text" name="subject" placeholder="Enter Your Subject">
-                </div>
-                <div class="form-group">
-                    <label>Email</label>
-                    <input type="email" name="email" placeholder="Enter Your Email">
-                </div>
-                <div class="form-group">
-                    <label>Massage</label>
-                    <textarea name="massage" cols="10" rows="10" class="form-control"></textarea>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+            </div>
 
         </div>
         {{-- Total earning Widthraw --}}
