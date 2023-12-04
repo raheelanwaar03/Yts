02d6c5e77b01f504dd6778cab0bbfbbac67b7f73
raheel0316 <raheelanwaar0316@gmail.com>
2023-12-04 12:28:52 -0800

    asdf

--- app/Http/Middleware/feesCheck.php
+++ app/Http/Middleware/feesCheck.php
@@ -21,8 +21,6 @@ class feesCheck
         }
         if (auth()->user()->status == 'rejected') {
             return redirect()->route('Registeration.Fees')->with('error', 'Your accound has been rejected Please pay your registeration fees and enter correct Tid & if you pay us before then enter correct tid username and number again');
-        } else {
-            return redirect()->back()->with('success', 'Your details has been submited successfully our team will review your details and approve your account if given information is valid!');
         }
     }
 }
