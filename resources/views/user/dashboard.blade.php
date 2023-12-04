65e51a9d6a23ecfb9f1fa705e0b1f430b5cf5ed8
raheel0316 <raheelanwaar0316@gmail.com>
2023-12-04 12:49:10 -0800

    into the container

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -647,34 +647,26 @@
         <div class="text-center hidden" id="contact">
             contact us
             <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="card">
-                            <div class="card-body">
-                                <form action="mailto:asraah455@gmail.com?subject=Mail from Site!" method="post">
-                                    @csrf
-                                    <div class="form-group">
-                                        <label>Name</label>
-                                        <input type="text" name="name" placeholder="Enter Your Name">
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Subject</label>
-                                        <input type="text" name="subject" placeholder="Enter Your Subject">
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Email</label>
-                                        <input type="email" name="email" placeholder="Enter Your Email">
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Massage</label>
-                                        <textarea name="massage" cols="10" rows="10" class="form-control"></textarea>
-                                    </div>
-                                    <button type="submit" class="btn btn-primary">Submit</button>
-                                </form>
-                            </div>
-                        </div>
+                <form action="mailto:asraah455@gmail.com?subject=Mail from Site!" method="post">
+                    @csrf
+                    <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" name="name" placeholder="Enter Your Name">
                     </div>
-                </div>
+                    <div class="form-group">
+                        <label>Subject</label>
+                        <input type="text" name="subject" placeholder="Enter Your Subject">
+                    </div>
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input type="email" name="email" placeholder="Enter Your Email">
+                    </div>
+                    <div class="form-group">
+                        <label>Massage</label>
+                        <textarea name="massage" cols="10" rows="10" class="form-control"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
             </div>
 
         </div>
