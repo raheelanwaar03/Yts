4b91462df2a35515836f337f1e39e29b6ef14bc7
raheel0316 <raheelanwaar0316@gmail.com>
2023-12-05 13:24:45 -0800

    new addition

--- resources/views/admin/setting/level.blade.php
+++ resources/views/admin/setting/level.blade.php
@@ -1,64 +1,170 @@
 @extends('admin.layout.app')
 
 @section('content')
-    <div class="content-body">
-        <!-- row -->
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4 class="card-title">Level Settings</h4>
+    <div class="dasboard-body">
+        <div class="dashboard-hero">
+            <div class="header-top" style="background-color: tomato !important">
+                <div class="container">
+                    <div class="mobile-header d-flex justify-content-between d-lg-none align-items-center">
+                        <div class="author">
+                            <img src="{{ asset('assets/images/dashboard/author.png') }}" alt="dashboard">
                         </div>
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table id="example" class="display" style="min-width: 845px">
-                                    <thead>
-                                        <tr>
-                                            <th>Level 1</th>
-                                            <th>Level 2</th>
-                                            <th>Level 3</th>
-                                            <th>Level 4</th>
-                                            <th>Level 5</th>
-                                            <th>Level 6</th>
-                                            <th>Level 7</th>
-                                            <th>Level 8</th>
-                                            <th>Level 9</th>
-                                            <th>Level 10</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($levels as $level)
-                                            <tr>
-                                                <td>{{ $level->level1 }}</td>
-                                                <td>{{ $level->level2 }}</td>
-                                                <td>{{ $level->level3 }}</td>
-                                                <td>{{ $level->level4 }}</td>
-                                                <td>{{ $level->level5 }}</td>
-                                                <td>{{ $level->level6 }}</td>
-                                                <td>{{ $level->level7 }}</td>
-                                                <td>{{ $level->level8 }}</td>
-                                                <td>{{ $level->level9 }}</td>
-                                                <td>{{ $level->level10 }}</td>
-                                                <td>
-                                                    <a href="{{ route('Admin.Edit.Level', ['id' => $level->id]) }}"
-                                                        class="btn btn-primary">Edit</a>
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
+                        {{-- <div class="cross-header-bar">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div> --}}
+                    </div>
+                    <div class="mobile-header-content d-lg-flex flex-wrap justify-content-lg-between align-items-center">
+                        <ul class="support-area">
+                            <li>
+                                <a href="#0"><i class="flaticon-support"></i>Support</a>
+                            </li>
+                            <li>
+                                <i class="flaticon-globe"></i>
+                            </li>
+                        </ul>
+                        <div
+                            class="dashboard-header-right d-flex flex-wrap justify-content-center justify-content-sm-between justify-content-lg-end align-items-center">
+                            <ul class="dashboard-right-menus">
+                                <li>
+                                    <a href="#0">
+                                    </a>
+                                    <div class="notification-area">
+                                        <div class="notifacation-header d-flex flex-wrap justify-content-between">
+                                        </div>
+                                        <div class="notifacation-footer text-center">
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a href="#0" class="author">
+                                        <div class="thumb">
+                                            <img src="{{ asset('assets/images/dashboard/author.png') }}" alt="dashboard">
+                                            <span class="checked">
+                                                <i class="flaticon-checked"></i>
+                                            </span>
+                                        </div>
+                                        <div class="content">
+                                            <h6 class="title">{{ auth()->user()->name }}</h6>
+                                            <span class="country">{{ auth()->user()->country }}</span>
+                                        </div>
+                                    </a>
+                                    <div class="notification-area">
+                                        <div class="author-header">
+                                            <div class="thumb">
+                                                <img src="{{ asset('assets/images/dashboard/author.png') }}"
+                                                    alt="dashboard">
+                                            </div>
+                                            <h6 class="title">{{ auth()->user()->name }}</h6>
+                                            <a href="#mailto:johndoe@gmail.com"><span class="__cf_email__"
+                                                    data-cfemail="5d173235333932381d3a303c3431733e3230">[email&#160;protected]</span></a>
+                                        </div>
+                                        <div class="author-body">
+                                            <ul>
+                                                <li>
+                                                    <a href="{{ route('profile.edit') }}" class="text-seconday"><i
+                                                            class="fas fa-user-edit"></i>Edit
+                                                        Profile</a>
+                                                </li>
+                                                <li>
+                                                    <form action="{{ route('logout') }}" method="POST">
+                                                        @csrf
+                                                        <button type="submit" class="btn btn-danger"><i
+                                                                class="fas fa-sign-out-alt"></i>Log
+                                                            Out</button>
+                                                    </form>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
-                <div class="footer">
-                    <div class="copyright">
-                        <p>Copyright © Designed &amp; Developed by <a href="#">
-                                {{ env('APP_NAME') }}</a> 2022</p>
+            </div>
+            <div class="dashboard-hero-content text-white" style="background-color: tomato !important">
+                <h3> <a href="/Admin/Dashboard"> Go Back to Home</a></h3>
+                <ul class="">
+                    <li>
+                        Dashboard
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="content-body">
+            <!-- row -->
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4 class="card-title">Level Settings</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table id="example" class="display" style="min-width: 845px">
+                                        <thead>
+                                            <tr>
+                                                <th>Level 1</th>
+                                                <th>Level 2</th>
+                                                <th>Level 3</th>
+                                                <th>Level 4</th>
+                                                <th>Level 5</th>
+                                                <th>Level 6</th>
+                                                <th>Level 7</th>
+                                                <th>Level 8</th>
+                                                <th>Level 9</th>
+                                                <th>Level 10</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($levels as $level)
+                                                <tr>
+                                                    <td>{{ $level->level1 }}</td>
+                                                    <td>{{ $level->level2 }}</td>
+                                                    <td>{{ $level->level3 }}</td>
+                                                    <td>{{ $level->level4 }}</td>
+                                                    <td>{{ $level->level5 }}</td>
+                                                    <td>{{ $level->level6 }}</td>
+                                                    <td>{{ $level->level7 }}</td>
+                                                    <td>{{ $level->level8 }}</td>
+                                                    <td>{{ $level->level9 }}</td>
+                                                    <td>{{ $level->level10 }}</td>
+                                                    <td>
+                                                        <a href="{{ route('Admin.Edit.Level', ['id' => $level->id]) }}"
+                                                            class="btn btn-primary">Edit</a>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer">
+                        <div class="copyright">
+                            <p>Copyright © Designed &amp; Developed by <a href="#">
+                                    {{ env('APP_NAME') }}</a> 2022</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    @endsection
+    </div>
+    </div>
+    </section>
+@endsection
+
+@section('datatable')
+    <script>
+        $(document).ready(function() {
+            $('#myTable').DataTable();
+        });
+    </script>
+    <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+@endsection
