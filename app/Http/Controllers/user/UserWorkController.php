17d09e61ecd105c7bfd066110745053dcdcb2233
raheel0316 <raheelanwaar0316@gmail.com>
2023-12-08 02:56:37 -0800

    asdf

--- app/Http/Controllers/user/UserWorkController.php
+++ app/Http/Controllers/user/UserWorkController.php
@@ -38,11 +38,11 @@ class UserWorkController extends Controller
             return redirect()->back()->with('error', 'You have not enough balance');
         }
 
-        if (auth()->user()->level = 'Level 1') {
+        if (auth()->user()->level == 'Level 1') {
             if ($validated['widthraw_amount'] >= 100) {
                 return redirect()->back()->with('error', 'You can only widthraw 100 Rs.');
             }
-        } elseif (auth()->user()->level != 'Level 1') {
+        } elseif (auth()->user()->level !== 'Level 1') {
             if ($validated['widthraw_amount'] >= 400) {
                 return redirect()->back()->with('error', 'You can only widthraw 400 Rs.');
             }
